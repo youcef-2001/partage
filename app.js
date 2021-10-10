2edc80369e6b5a27a11beb07a34e4cb87746c40d
youcef-2001 <ybaleh13@gmail.com>
2021-10-10 22:59:01 +0100

    second commit

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ console.log("git mis a jour");
 console.log(2545);
 let tab={"salam":20,"sousou":256};
 console.log("zumzul");
+let p1=document.querySelector("p");
+p1.innerText="added by java hey!!";
